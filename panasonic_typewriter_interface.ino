7a75f6d58181dd164eb765958177befe1de2b4e7
Matthew Nielsen <xunker@pyxidis.org>
2024-08-29 11:51:30 -0600

    bring over all changes from serial-bridge

--- panasonic_typewriter_interface.ino
+++ panasonic_typewriter_interface.ino
@@ -85,42 +85,7 @@ Verify the voltages of ALL PINS before connecting typerwiter to your device.
 SIGNAL_SETTLE_DELAY: Used in waitForSignalToSettle(), see that function for
 explaination.
 */
-<<<<<<< HEAD
-char messages[][54] = {
-  "This place is a message,",
-  "and part of a system of messages.",
-  "Pay attention to it!",
-  "",
-  "Sending this message was important to us.",
-  "We considered ourselves to be a powerful culture.",
-  "",
-  "This place is not a place of honor.",
-  "No highly esteemed deed is commemorated here.",
-  "Nothing valued is here.",
-  "",
-  "What is here was dangerous and repulsive to us.",
-  "This message is a warning about danger.",
-  "",
-  "The danger is in a particular location.",
-  "It increases towards a center.",
-  "The center of danger is here,",
-  "of a particular size and shape, and below us.",
-  "",
-  "The danger is still present,",
-  "in your time, as it was in ours.",
-  "",
-  "The danger is to the body, and it can kill.",
-  "",
-  "The form of the danger is an emanation of energy.",
-  "",
-  "The danger is unleashed only if you substantially",
-  "disturb this place physically.",
-  "",
-  "This place is best shunned and left uninhabited."
-};
-=======
 #define SIGNAL_SETTLE_DELAY 2
->>>>>>> serial-bridge
 
 /*
 How many MILLIseconds to wait after sending a byte to the typewriter, to give it a chance to type
